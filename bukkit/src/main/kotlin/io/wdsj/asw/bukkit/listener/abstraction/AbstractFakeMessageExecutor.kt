907efc65b4547f4a351f9d4b09ddf60a5d65fd1c
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2025-01-14 10:27:57 -0800

    Store with uuid

--- bukkit/src/main/kotlin/io/wdsj/asw/bukkit/listener/abstraction/AbstractFakeMessageExecutor.kt
+++ bukkit/src/main/kotlin/io/wdsj/asw/bukkit/listener/abstraction/AbstractFakeMessageExecutor.kt
@@ -1,27 +1,30 @@
 package io.wdsj.asw.bukkit.listener.abstraction
 
 import org.bukkit.entity.Player
+import java.util.UUID
 import java.util.concurrent.ConcurrentHashMap
 
 abstract class AbstractFakeMessageExecutor {
     protected fun shouldFakeMessage(player: Player): Boolean {
-        return FAKE_MESSAGE_NUM.getOrPut(player) { 0 } > 0
+        return FAKE_MESSAGE_NUM.getOrPut(player.uniqueId) { 0 } > 0
     }
 
     companion object {
         @JvmStatic
-        private val FAKE_MESSAGE_NUM = ConcurrentHashMap<Player, Int>()
+        private val FAKE_MESSAGE_NUM = ConcurrentHashMap<UUID, Int>()
         @JvmStatic
         fun selfDecrement(player: Player) {
-            val currentNum = FAKE_MESSAGE_NUM.getOrPut(player) { 0 }
+            val uuid = player.uniqueId
+            val currentNum = FAKE_MESSAGE_NUM.getOrPut(uuid) { 0 }
             if (currentNum > 0) {
-                FAKE_MESSAGE_NUM[player] = currentNum - 1
+                FAKE_MESSAGE_NUM[uuid] = currentNum - 1
             }
         }
 
         @JvmStatic
         fun selfIncrement(player: Player) {
-            FAKE_MESSAGE_NUM[player] = FAKE_MESSAGE_NUM.getOrPut(player) { 0 } + 1
+            val uuid = player.uniqueId
+            FAKE_MESSAGE_NUM[uuid] = FAKE_MESSAGE_NUM.getOrPut(uuid) { 0 } + 1
         }
     }
 }
